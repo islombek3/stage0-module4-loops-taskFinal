6d11e0d24237bdfaee693dac3506577efba8d0c1
Islombek Akhmadjonov <81472194+islombek3@users.noreply.github.com>
2022-10-30 18:29:26 +0500

    add logic

--- src/main/java/school/mjc/stage0/loops/finalTask/MultiplicationTable.java
+++ src/main/java/school/mjc/stage0/loops/finalTask/MultiplicationTable.java
@@ -2,5 +2,11 @@ package school.mjc.stage0.loops.finalTask;
 
 public class MultiplicationTable {
     public void printTable(int numberTableToPrint){
+        int x = 1;
+        while (x < 11){
+            int result = x * numberTableToPrint;
+            System.out.println(x + " x " + numberTableToPrint + " = " + result);
+            x++;
+        }
     }
 }

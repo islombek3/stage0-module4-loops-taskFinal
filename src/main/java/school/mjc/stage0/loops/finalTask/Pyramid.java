173f1c709f7f8824aa1dbe5ce8d762d5b8ba9aea
Islombek Akhmadjonov <81472194+islombek3@users.noreply.github.com>
2022-10-30 18:30:56 +0500

    add logic

--- src/main/java/school/mjc/stage0/loops/finalTask/Pyramid.java
+++ src/main/java/school/mjc/stage0/loops/finalTask/Pyramid.java
@@ -2,7 +2,19 @@ package school.mjc.stage0.loops.finalTask;
 
 public class Pyramid {
     public void printPyramid(int cathetusLength) {
-
+        String empty = " ";
+        int l = cathetusLength, x = 1;
+        String xString = "";
+        while (l > 0){
+            if (l == cathetusLength){
+                xString = "" + x + xString;
+            } else {
+                xString = "" + x + xString + x;
+            }
+            System.out.println(empty.repeat(l-1) + xString);
+            l--;
+            x++;
+        }
     }
 
     public static void main(String[] args) {

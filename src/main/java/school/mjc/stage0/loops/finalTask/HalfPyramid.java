c8129cb6111b9b9de67057a9dd4caee29b51bb06
Islombek Akhmadjonov <81472194+islombek3@users.noreply.github.com>
2022-10-30 18:28:45 +0500

    add logic

--- src/main/java/school/mjc/stage0/loops/finalTask/HalfPyramid.java
+++ src/main/java/school/mjc/stage0/loops/finalTask/HalfPyramid.java
@@ -2,7 +2,13 @@ package school.mjc.stage0.loops.finalTask;
 
 public class HalfPyramid {
     public void printHalfPyramid(int cathetusLength) {
-
+        String empty = " ", star = "*";
+        int i = cathetusLength - 1, x = 1;
+        while (x <= cathetusLength){
+            System.out.println(empty.repeat(i) + star.repeat(x));
+            i--; 
+            x++;
+        }
     }
 
     public static void main(String[] args) {
